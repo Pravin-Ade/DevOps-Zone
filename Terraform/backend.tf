c8a7f2bd73cd04fa3aabff8af647b7b684bafd70
Pravin Ade <65883891+Pravin-Ade@users.noreply.github.com>
2025-07-11 13:21:08 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,3 +1,8 @@
+# Old Way: Using DynamoDB for Locking
+  # Store the state in an S3 bucket.
+  # Create a DynamoDB table with a LockID key.
+  # Reference the table in your backend config.
+
 terraform {
     backend "s3" {
         bucket = "bucket_name"
@@ -7,6 +12,19 @@ terraform {
         encrypt = true
     }
 }
+
+# New Way: Native S3 Locking (Terraform ≥ 1.10)
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true # you can enable locking directly in S3 using the use_lockfile = true parameter.
+  }
+}
+
 
 ### terraform init
 
